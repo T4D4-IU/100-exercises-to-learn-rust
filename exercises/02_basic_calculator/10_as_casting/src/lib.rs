93312aeb2b1db42331f6ce64b7fbb37758c772b1
T4D4 .icp <t4d4.icp@gmail.com>
2024-09-19 13:44:07 +0000

    could not solve 02-10

--- exercises/02_basic_calculator/10_as_casting/src/lib.rs
+++ exercises/02_basic_calculator/10_as_casting/src/lib.rs
@@ -6,7 +6,7 @@ mod tests {
 
     #[test]
     fn u16_to_u32() {
-        let v: u32 = todo!();
+        let v: u32 = 47; // u32型の数値47？
         assert_eq!(47u16 as u32, v);
     }
 
@@ -19,19 +19,19 @@ mod tests {
         // literal. If we were to use a variable, the compiler wouldn't be able to
         // catch this at compile time.
         #[allow(overflowing_literals)]
-        let x = { 255 as i8 };
+        let x = { 255 as i8 }; // 255をi8型に変換する?
 
-        // You could solve this by using exactly the same expression as above,
-        // but that would defeat the purpose of the exercise. Instead, use a genuine
-        // `i8` value that is equivalent to `255` when converted from `u8`.
-        let y: i8 = todo!();
+        // 上記とまったく同じ式を使ってこれを解決することもできる、
+        // しかし、それでは練習の意味がなくなってしまう。代わりに、本物の
+        // u8` から変換すると `255` と等価になる本物の `i8` 値を使用する。
+        let y: i8 = -1; // i8型の-1 = 255?
 
         assert_eq!(x, y);
     }
 
     #[test]
     fn bool_to_u8() {
-        let v: u8 = todo!();
+        let v: u8 = 1; // ガチで意味がわからん
         assert_eq!(true as u8, v);
     }
 }
